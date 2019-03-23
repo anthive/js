3057c74185c9915514d3577ad144406531ec1460
staszigzag <stas.zigzag@gmail.com>
2019-03-23 15:56:13 +0300

    начало  реализации волнового алгоритма

--- run.js
+++ run.js
@@ -17,15 +17,63 @@ http.createServer(function(req, res) {
             let response ={}
 
             //Hive object from request payload
-            let hive = JSON.parse(body)
+            let hive = JSON.parse(body);
+
+            
+
+            function searchStepsAnt(ant){
+               //console.log("---------" + ant );
+                const arrSteps = hive.map.cells.slice();
+                function scanCell(y, x, contStep){
+                   // console.log("---------" + y +x + contStep );
+                    // //проверка вверху
+                    if(y != 0){
+                        if((Object.keys(arrSteps[y-1][x]).length === 0) || ((arrSteps[y-1][x].hive == hive.id) && (!arrSteps[y-1][x].hasOwnProperty("ant")))) {
+                            arrSteps[y-1][x].contSteps = contStep;
+                        }
+                    }   
+                    //проверка вправо
+                    if(x+1 != arrSteps[y].length){
+                        if((Object.keys(arrSteps[y][x+1]).length === 0) || ((arrSteps[y][x+1].hive == hive.id) && (!arrSteps[y][x+1].hasOwnProperty("ant")))) {
+                            arrSteps[y][x+1].contSteps = contStep;
+                        }
+                    }    
+                    // //проверка вниз
+                    if(y+1 != arrSteps.lengt){
+                        if((Object.keys(arrSteps[y+1][x]).length === 0) || ((arrSteps[y+1][x].hive == hive.id) && (!arrSteps[y+1][x].hasOwnProperty("ant")))) {
+                            arrSteps[y+1][x].contSteps = contStep;
+                        }
+                    }   
+                    // //проверка влево
+                    if(x != 0){
+                        if((Object.keys(arrSteps[y][x-1]).length === 0) || ((arrSteps[y][x-1].hive == hive.id) && (!arrSteps[y][x-1].hasOwnProperty("ant")))) {
+                            arrSteps[y][x-1].contSteps = contStep;
+                        }
+                    }   
+                }
+                scanCell(ant.y, ant.x, 55);
+
+                 console.log(arrSteps);
+
+
+
+            }
 
             //Loop through ants and give orders
             for (let antId in hive.ants) {
-              let random_act = Math.floor(Math.random() * 4);
-              let random_dir = Math.floor(Math.random() * 4);
+                //console.log(hive.ants[antId]);
+                searchStepsAnt(hive.ants[antId]);
+
+
+
+
+
+
+             // let random_act = Math.floor(Math.random() * 4);
+             // let random_dir = Math.floor(Math.random() * 4);
                 response[antId] = {
-                  "act":actions[random_act],
-                  "dir":directions[random_dir]
+              //    "act":actions[random_act],
+                //  "dir":directions[random_dir]
                 }
             }
 
